14c44196b1894aba80e4980569e5cbd8ab4f7aa2
Clément Binet <Clement@Imac-de-Clement-4.local>
2026-05-19 17:03:45 +0200

    puppeteer fix

--- index.js
+++ index.js
@@ -39,7 +39,26 @@ const client =
 
     puppeteer: {
 
-      headless: true
+      headless: true,
+
+      args: [
+
+        '--no-sandbox',
+
+        '--disable-setuid-sandbox',
+
+        '--disable-dev-shm-usage',
+
+        '--disable-accelerated-2d-canvas',
+
+        '--no-first-run',
+
+        '--no-zygote',
+
+        '--single-process',
+
+        '--disable-gpu'
+      ]
     }
   })
 
